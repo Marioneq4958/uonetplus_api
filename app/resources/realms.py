7ffa1d2d627405daf7acbab5713fd77e8458977e
Marioneq4958 <marioneq4958@gmail.com>
2022-06-15 14:40:37 +0200

    Add realms

---
+++ app/resources/realms.py
@@ -0,0 +1,150 @@
+REALMS = [
+    {
+        "name": "Vulcan UONET+",
+        "signin_type": "CUFS",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "Default"
+    },
+    {
+        "name": "Opolska eSzkoła",
+        "signin_type": "ADFS",
+        "host": "uonetplus.eszkola.opolskie.pl",
+        "ssl": True,
+        "symbol": "Default"
+    },
+    {
+        "name": "Gdańska Platforma Edukacyjna",
+        "signin_type": "ADFS",
+        "host": "edu.gdansk.pl",
+        "ssl": True,
+        "symbol": "gdansk"
+    },
+    {
+        "name": "Rzeszów ResMan2",
+        "signin_type": "ADFS_LIGHT",
+        "host": "resman.pl",
+        "ssl": True,
+        "symbol": "rzeszow"
+    },
+    {
+        "name": "Portal Edukacyjny Miasta Koszalina",
+        "signin_type": "ADFS_PORTAL",
+        "host": "eduportal.koszalin.pl",
+        "ssl": True,
+        "symbol": "rzeszow"
+    },
+    {
+        "name": "Tarnów - EduNet eSzkoła",
+        "signin_type": "ADFS_PORTAL",
+        "host": "umt.tarnow.pl",
+        "ssl": True,
+        "symbol": "tarnow"
+    },
+    {
+        "name": "Lubelski Portal Oświatowy",
+        "signin_type": "ADFS_LIGHT",
+        "host": "edu.lublin.eu",
+        "ssl": True,
+        "symbol": "lublin"
+    },
+    {
+        "name": "Sieradz - Portal Oświatowy",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "vulcan.net.pl"
+    },
+    {
+        "name": "Rawa Mazowiecka - Platforma vEdukacja",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "rawamazowiecka"
+    },
+    {
+        "name": "Łask - Platforma vEdukacja",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "lask"
+    },
+    {
+        "name": "",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": ""
+    },
+    {
+        "name": "Powiat Kętrzyński - Platforma e-usług",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "powiatketrzynski"
+    },
+    {
+        "name": "Powiat łaski - platforma edukacyjna",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "powiatlaski"
+    },
+    {
+        "name": "Zduńska Wola - e-urząd",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "zdunskawola"
+    },
+    {
+        "name": "Gmina Ozorków",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "gminaozorkow"
+    },
+
+    {
+        "name": "Gmina Ulan-Majorat",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "gminaulanmajorat"
+    },
+    {
+        "name": "Powiat Krasnostawski",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "powiatkrasnostawski"
+    },
+    {
+        "name": "Gmina Łopiennik Górny",
+        "signin_type": "ADFS_LIGHT",
+        "host": "vulcan.net.pl",
+        "ssl": True,
+        "symbol": "gminalopiennikgorny"
+    },
+    {
+        "name": "FakelogCF",
+        "signin_type": "CUFS",
+        "host": "fakelog.cf",
+        "ssl": False,
+        "symbol": "Default"
+    },
+    {
+        "name": "FakelogTK",
+        "signin_type": "CUFS",
+        "host": "fakelog.tk",
+        "ssl": False,
+        "symbol": "Default"
+    },
+    {
+        "name": "FakelogGQ",
+        "signin_type": "CUFS",
+        "host": "fakelog.gq",
+        "ssl": True,
+        "symbol": "Default"
+    },
+]
