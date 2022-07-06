6072a1b37bece2b46eeed585b68585a1119f1ce0
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-07-06 18:07:27 +0200

    Add block check

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,7 @@ class fg:
 class Settings(BaseSettings):
   API_HOST: str = "127.0.0.1"
   API_PORT: int = 8000
+  API_DEBUG: bool = False
   API_V1_URL: str = "/api/v1"
   PYTHON_VER = platform.python_version()
   if PYTHON_VER == "3.10.4" or PYTHON_VER == "3.10.5":
@@ -23,9 +24,9 @@ class Settings(BaseSettings):
     raise Exception(f'{fg.red}Twoja wersja pythona to: {PYTHON_VER} a potrzebna jest 3.10.4 lub wyższa!!!{fg.rs}')
   TESTS_USERNAME: EmailStr = "jan@fakelog.cf"
   TESTS_PASSWORD: str = "jan123"
-  TESTS_HOST: str = "fakelog.cf"
+  TESTS_HOST: str = "fakelog.gq"
   TESTS_BACKUP_HOST: str = "fakelog.tk"
-  TESTS_SSL: bool = False
+  TESTS_SSL: bool = True
   TESTS_INVALID_PASSWORD: str = "marzenna123"
   TESTS_SEMESTER: str = "16"
   TESTS_DEVICE_ID: int = 1234
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import uvicorn
-
+class fg:
+    lightgreen = "\x1B[38;5;46m"
+    orange = "\x1B[38;5;208m"
+    red = "\x1B[38;5;160m"
+    rs = "\033[0m"
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from cryptography.fernet import Fernet
 from app.routes import auth, uonetplus_uczen, github
 from app.core.config import settings
-
-app = FastAPI(title="Uonetplus API", version="0.3.0")
+if settings.API_DEBUG == True:
+    print(f'{fg.red}UWAGA!!! ZAŁĄCZONO TRYB DEBUG!!! DOKUMENTACJA OPENAPI ODBLOKOWANA!!!{fg.rs}')
+    app = FastAPI(title="Uonetplus API", version="0.3.0")
+else:
+    print(f'{fg.lightgreen}TRYB DEBUG WYŁĄCZONY!{fg.rs}')
+    app = FastAPI(title="Uonetplus API", version="0.3.0", openapi_url=None, docs_url=None, redoc_url=None)
 
 app.include_router(auth.router, prefix=settings.API_V1_URL + "/auth", tags=["auth"])
 app.include_router(uonetplus_uczen.router, prefix=settings.API_V1_URL + "/uonetplus-uczen", tags=["uonetplus-uczen"])
@@ -25,4 +33,7 @@ app.add_middleware(
 app.add_middleware(SessionMiddleware, secret_key=secret_key)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=settings.API_HOST, port=settings.API_PORT, reload=True)
+    if settings.API_DEBUG == True:
+        uvicorn.run("main:app", host=settings.API_HOST, port=settings.API_PORT, reload=True)
+    else:
+        uvicorn.run("main:app", host=settings.API_HOST, port=settings.API_PORT)
